ac71d55f9196a232076e66ee8604fc4c19a608fc
kvelraj_concord <kvelraj@concord.net>
2021-07-27 22:01:01 +0530

    initial

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.json());
+
+const studentList = [{
+  firstName : "Yuvaraj",
+  lastName : "A",
+  rollNo : 4056,
+  age : 28
+},{
+  firstName : "Karthic",
+  lastName : "K",
+  rollNo : 4550,
+  age : 29
+},{
+  firstName : "Aswin",
+  lastName : "K",
+  rollNo : 4012,
+  age : 22
+}];
+
+app.get('/api/students', (request, response) =>{
+  response.status(200).send(studentList);
+});
+
+const port = process.env.port || 8080;
+
+http.listen(port, () => {
+  console.log("SERVER IS RUNNING ON PORT 8080");
+})
