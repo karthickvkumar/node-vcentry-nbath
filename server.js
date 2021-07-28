b149b71cb9a5d002f44f466bfe0d953d034bab5f
kvelraj_concord <kvelraj@concord.net>
2021-07-28 22:28:08 +0530

    update

--- server.js
+++ server.js
@@ -10,17 +10,20 @@ const studentList = [{
   firstName : "Yuvaraj",
   lastName : "A",
   rollNo : 4056,
-  age : 28
+  age : 28,
+  id : 1
 },{
   firstName : "Karthic",
   lastName : "K",
   rollNo : 4550,
-  age : 29
+  age : 29,
+  id : 2
 },{
   firstName : "Aswin",
   lastName : "K",
   rollNo : 4012,
-  age : 22
+  age : 22,
+  id : 3
 }];
 
 app.get('/api/students', (request, response) =>{
@@ -32,7 +35,8 @@ app.post('/api/student/create', (request, response) => {
     firstName : request.body.firstName,
     lastName : request.body.lastName,
     rollNo : request.body.rollNo,
-    age : request.body.age
+    age : request.body.age,
+    id : studentList.length + 1
   };
 
   studentList.push(newStudent);
@@ -42,6 +46,42 @@ app.post('/api/student/create', (request, response) => {
   }
   response.status(200).send(data);
 });
+
+
+app.put('/api/student/update/:id', (request, response) => {
+  const id = request.params.id;
+  const index = studentList.findIndex((value, index) => {
+    return value.id == id;
+  });
+
+  const editStudent = {
+    firstName : request.body.firstName,
+    lastName : request.body.lastName,
+    rollNo : request.body.rollNo,
+    age : request.body.age,
+    id : studentList[index].id
+  };
+
+  studentList[index] = editStudent;
+  const data = {
+    message : "Student has been Updated successfully"
+  }
+  response.status(200).send(data);
+});
+
+app.delete('/api/student/delete/:id', (request, response) => {
+  const id = request.params.id;
+  const index = studentList.findIndex((value, index) => {
+    return value.id == id;
+  });
+
+  studentList.splice(index, 1);
+  const data = {
+    message : "Student has been Deleted successfully"
+  }
+  response.status(200).send(data);
+})
+
 
 const port = process.env.port || 8080;
 
