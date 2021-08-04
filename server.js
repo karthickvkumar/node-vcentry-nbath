9780f64c364d8c2f08c6af40ac0f17ad2203cc8f
kvelraj_concord <kvelraj@concord.net>
2021-08-04 21:52:54 +0530

    sql

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const http = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
+const DB_NAME = "student_test_db_100";
+const TABLE_NAME = "student_info";
+
 app.use(bodyParser.json());
 app.use(express.json());
 
@@ -22,80 +25,38 @@ connection.connect((error) => {
   console.log('Successfully connected the SQL Server')
 });
 
-const studentList = [{
-  firstName : "Yuvaraj",
-  lastName : "A",
-  rollNo : 4578,
-  age : 28,
-  id : 1
-},{
-  firstName : "Karthic",
-  lastName : "K",
-  rollNo : 4550,
-  age : 29,
-  id : 2
-},{
-  firstName : "Aswin",
-  lastName : "K",
-  rollNo : 4012,
-  age : 22,
-  id : 3
-}];
+app.post('/api/create/db', (request, response) => {
+  connection.query(`CREATE DATABASE ${DB_NAME}`, (error, result) => {
+    if(error){
+      response.status(500).send({
+        message : "Problem in creating new database",
+        error
+      });
+      return;
+    }
+
+    response.status(200).send({
+      message : "Created a new Database",
+      result
+    });
+  })
+})
 
 app.get('/api/students', (request, response) =>{
-  response.status(200).send(studentList);
+ 
 });
 
 app.post('/api/student/create', (request, response) => {
-  const newStudent = {
-    firstName : request.body.firstName,
-    lastName : request.body.lastName,
-    rollNo : request.body.rollNo,
-    age : request.body.age,
-    id : studentList.length + 1
-  };
 
-  studentList.push(newStudent);
-
-  const data = {
-    message : "Student has been created successfully"
-  }
-  response.status(200).send(data);
 });
 
 
 app.put('/api/student/update/:id', (request, response) => {
-  const id = request.params.id;
-  const index = studentList.findIndex((value, index) => {
-    return value.id == id;
-  });
 
-  const editStudent = {
-    firstName : request.body.firstName,
-    lastName : request.body.lastName,
-    rollNo : request.body.rollNo,
-    age : request.body.age,
-    id : studentList[index].id
-  };
-
-  studentList[index] = editStudent;
-  const data = {
-    message : "Student has been Updated successfully"
-  }
-  response.status(200).send(data);
 });
 
 app.delete('/api/student/delete/:id', (request, response) => {
-  const id = request.params.id;
-  const index = studentList.findIndex((value, index) => {
-    return value.id == id;
-  });
 
-  studentList.splice(index, 1);
-  const data = {
-    message : "Student has been Deleted successfully"
-  }
-  response.status(200).send(data);
 })
 
 
