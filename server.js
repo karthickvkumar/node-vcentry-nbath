9fa969dafc0381b874c249b4f6b049dedf9987d2
kvelraj_concord <kvelraj@concord.net>
2021-08-06 21:28:24 +0530

    sql

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
-const DB_NAME = "student_test_db_100";
+const DB_NAME = "student_test_db";
 const TABLE_NAME = "student_info";
 
 app.use(bodyParser.json());
@@ -13,7 +13,8 @@ app.use(express.json());
 const connection = mysql.createConnection({
   host : 'localhost',
   user : 'root',
-  password : ''
+  password : '',
+  database: DB_NAME,
 });
 
 connection.connect((error) => {
@@ -40,16 +41,66 @@ app.post('/api/create/db', (request, response) => {
       result
     });
   })
+});
+
+
+app.post('/api/create/table', (request, response) => {
+ const query = `CREATE TABLE ${TABLE_NAME} (first_name varchar(255), last_name varchar(255), age int(3), id int(3) NOT NULL AUTO_INCREMENT, PRIMARY KEY(id))`;
+
+ connection.query(query, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+      return;
+    }
+
+    response.status(200).send("Table has been created successfully");
+ })
 })
 
-app.get('/api/students', (request, response) =>{
- 
-});
-
 app.post('/api/student/create', (request, response) => {
+  const firstName = request.body.first_name;
+  if(!firstName){
+    response.status(400).send("Invalid or Missing First Name");
+    return;
+  }
+
+  const lastName = request.body.last_name;
+  if(!lastName){
+    response.status(400).send("Invalid or Missing Last Name");
+    return;
+  }
+
+  const age = request.body.age;
+  if(!age){
+    response.status(400).send("Invalid or Missing Age");
+    return;
+  }
+
+  const query = `INSERT INTO ${TABLE_NAME} (first_name, last_name, age) VALUES ('${firstName}', '${lastName}', ${age})`;
+
+  connection.query(query, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+      return;
+    }
+
+    response.status(200).send("New user profile has been created successfully");
+  })
 
 });
 
+app.get('/api/students', (request, response) =>{
+  const query = `SELECT * FROM ${TABLE_NAME}`
+
+  connection.query(query, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+      return;
+    }
+
+    response.status(200).send(result);
+  })
+});
 
 app.put('/api/student/update/:id', (request, response) => {
 
